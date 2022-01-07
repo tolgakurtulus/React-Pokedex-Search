8536c41edc09f89a99f55feed1ae2134d0810f30
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-07 17:25:08 +0300

    Formetter Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 import Form from "./components/Form";
 import CardList from "./components/CardList";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
-
 
   useEffect(() => {
     axiosGet();
   });
 
-  
-
   const axiosGet = () => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=898`)
-    .then(response => {
-      setItems(response.data.results)
-    })
-    .catch(error => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/?limit=898`)
+      .then((response) => {
+        setItems(response.data.results);
+      })
+      .catch((error) => {
         console.log(error);
-    });
-  }
-
-
+      });
+  };
 
   return (
     <div className="App c-app">
       <header>
         <h1>Pokemon Find</h1>
       </header>
-      <Form 
-        inputText = {inputText}
-        setInputText = {setInputText}
-      />
-      <CardList
-        items = {items}
-        inputText = {inputText}
-      />
+      <Form inputText={inputText} setInputText={setInputText} />
+      <CardList items={items} inputText={inputText} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Form = ({ setInputText, inputText }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
 
-    const inputTextHandler = (e) => {
-        setInputText(e.target.value)
-    }
-
-    return (
-
-        <form className="c-form">
-            <input
-                type="text"
-                className="form-control c-todo-input"
-                id="InputName"
-                aria-describedby="pokemon"
-                placeholder="Pokemon Name"
-                value={inputText}
-                onChange={inputTextHandler}
-            />
-        </form>
-    )
-}
+  return (
+    <form className="c-form">
+      <input
+        type="text"
+        className="form-control c-todo-input"
+        id="InputName"
+        aria-describedby="pokemon"
+        placeholder="Pokemon Name"
+        value={inputText}
+        onChange={inputTextHandler}
+      />
+    </form>
+  );
+};
 
 export default Form;
